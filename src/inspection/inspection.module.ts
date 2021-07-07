db9aaa0bc1799286bf4bc91c29cdcf4b4d675c2d
ST <yst@laonpeople.com>
2021-07-07 13:37:24 +0900

    Complete inspection module

--- src/inspection/inspection.module.ts
+++ src/inspection/inspection.module.ts
@@ -1,4 +1,14 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
+import { InspectionController } from './inspection.controller';
+import { InspectionService } from './inspection.service';
+import { Inspection, InspectionSchema } from './schema/inspection.schema';
 
-@Module({})
+@Module({
+  imports: [
+    MongooseModule.forFeature([{ name: Inspection.name, schema: InspectionSchema }]),
+  ],
+  controllers: [InspectionController],
+  providers: [InspectionService],
+})
 export class InspectionModule {}
