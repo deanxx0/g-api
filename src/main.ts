00112deab656e9a7df7be4b25cca64555bae55f6
ST <yst@laonpeople.com>
2021-07-15 16:57:45 +0900

    Extend json request file size limit

--- src/main.ts
+++ src/main.ts
@@ -4,8 +4,8 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.use(json({ limit: '5mb' }));
-  app.use(urlencoded({ extended: true, limit: '5mb' }));
+  app.use(json({ limit: '10mb' }));
+  app.use(urlencoded({ extended: true, limit: '10mb' }));
   await app.listen(parseInt(process.env.SERVER_PORT) || 3000);
 }
 bootstrap();
