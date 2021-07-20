4531c3cb6b71c707d594f16865fa3ac1daf01281
Sangwoo <swp@laonpeople.com>
2021-07-20 11:10:36 +0900

    Enable CORS

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,7 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors({ origin: '*' });
   app.use(json({ limit: '10mb' }));
   app.use(urlencoded({ extended: true, limit: '10mb' }));
   await app.listen(parseInt(process.env.SERVER_PORT) || 3000);
