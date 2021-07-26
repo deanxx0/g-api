124d74df924b37a7d63ef6a67430f01516a6c8ed
ST <yst@laonpeople.com>
2021-07-26 17:12:54 +0900

    Create result info page schema

---
+++ src/inspection/schema/result-info.schema.ts
@@ -0,0 +1,46 @@
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { Document } from 'mongoose';
+import { FinalResult } from '../enum/final-result';
+
+export type ResultInfoDocument = ResultInfo & Document;
+
+@Schema({ timestamps: true })
+export class ResultInfo {
+  @Prop()
+  inspectionNo: number;
+
+  @Prop()
+  startTime: Date;
+
+  @Prop()
+  endTime: Date;
+
+  @Prop()
+  elapseTime: Date;
+  
+  @Prop()
+  vehicleModel: String;
+  
+  @Prop()
+  vehicleColor: String;
+  
+  @Prop()
+  vinCode: String;
+
+  @Prop()
+  totalDefects: number;
+
+  @Prop()
+  totalSpecialDefects: number;
+
+  @Prop()
+  totalGapDefects: number;
+
+  @Prop()
+  finalResult: FinalResult;
+
+  @Prop()
+  inspectionStatus: String;
+}
+
+export const ResultInfoSchema = SchemaFactory.createForClass(ResultInfo);
