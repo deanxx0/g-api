8a8d97e96b46880b66b766e5e7b5e3fabcf58f10
ST <yst@laonpeople.com>
2021-08-13 17:01:39 +0900

    Apply prettier

--- src/defectMap/defect-map.service.ts
+++ src/defectMap/defect-map.service.ts
@@ -42,17 +42,34 @@ export class DefectMapService {
           let finalX: number = 0;
           let finalY: number = 0;
           if (camera.groups[1] == 'LEFT' && camera.groups[2] == 'SIDE') {
-            finalX = ir.grab.distance + rotatedPoint.x * camera.resolution * RESOLUTION_MODIFIER - camera.x * 10;
-            finalY = camera.z * 10 - rotatedPoint.y * camera.resolution * RESOLUTION_MODIFIER;
+            finalX =
+              ir.grab.distance +
+              rotatedPoint.x * camera.resolution * RESOLUTION_MODIFIER -
+              camera.x * 10;
+            finalY =
+              camera.z * 10 -
+              rotatedPoint.y * camera.resolution * RESOLUTION_MODIFIER;
           } else if (
             camera.groups[1] == 'RIGHT' &&
             camera.groups[2] == 'SIDE'
           ) {
-            finalX = ir.grab.distance - rotatedPoint.x * camera.resolution * RESOLUTION_MODIFIER - camera.x * 10;
-            finalY = 8000 - camera.z * 10 + rotatedPoint.y * camera.resolution * RESOLUTION_MODIFIER;
+            finalX =
+              ir.grab.distance -
+              rotatedPoint.x * camera.resolution * RESOLUTION_MODIFIER -
+              camera.x * 10;
+            finalY =
+              8000 -
+              camera.z * 10 +
+              rotatedPoint.y * camera.resolution * RESOLUTION_MODIFIER;
           } else {
-            finalX = ir.grab.distance + rotatedPoint.x * camera.resolution * RESOLUTION_MODIFIER - camera.x * 10;
-            finalY = 2666 + camera.y * 10 - rotatedPoint.y * camera.resolution * RESOLUTION_MODIFIER;
+            finalX =
+              ir.grab.distance +
+              rotatedPoint.x * camera.resolution * RESOLUTION_MODIFIER -
+              camera.x * 10;
+            finalY =
+              2666 +
+              camera.y * 10 -
+              rotatedPoint.y * camera.resolution * RESOLUTION_MODIFIER;
           }
 
           finalPoints.push({ x: finalX, y: finalY });
