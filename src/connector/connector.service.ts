d6612a737ea6ec025ca93d89f82c2ccaea7afb1f
deanxx <deanxx999@gmail.com>
2021-08-27 11:12:20 +0900

    add return value to create inspection log function

--- src/connector/connector.service.ts
+++ src/connector/connector.service.ts
@@ -136,7 +136,7 @@ export class ConnectorService {
     createdLog.save();
   }
 
-  async createInspectionLog(inspectionId: string, status: string) {
+  async createInspectionLog(inspectionId: string, status: string): Promise<LogDocument> {
     const inspectionDoc: InspectionDocument = await this.inspectionModel
       .findOne({ _id: inspectionId })
       .exec();
@@ -148,7 +148,7 @@ export class ConnectorService {
     createdLog.type = 'INFO';
     createdLog.description = `InspectionNO: ${inspectionDoc.inspectionNo}, Status: ${status}, Vincode: ${inspectionDoc.vehicle.vinCode}, Model: ${inspectionDoc.vehicle.properties.model}, Color: ${inspectionDoc.vehicle.properties.color}`;
 
-    createdLog.save();
+    return await createdLog.save();
   }
 
   private inferencResultCache: Array<CreateInferenceResultDto> = [];
